540af545f1d0b0d30d53e581fb8f84a8dc536427
David Phillips <davidp6@uw.edu>
2017-03-30 14:42:25 -0700

    Add completeness to simulation probability graphs

--- Code/visualization/graph_simulation_probabilities.r
+++ Code/visualization/graph_simulation_probabilities.r
@@ -23,6 +23,7 @@ library(RColorBrewer)
 inFileAge = './Data/Simulation_Inputs/unspecified_age_probabilities.csv'
 inFileSex = './Data/Simulation_Inputs/unspecified_sex_probabilities.csv'
 inFileParity = './Data/Simulation_Inputs/unspecified_parity_probabilities.csv'
+inFileCompleteness = './Data/Simulation_Inputs/completeness_probabilities.csv'
 
 # output file
 outFile = './Visualizations/simulation_probabilities.pdf'
@@ -36,19 +37,22 @@ outFile = './Visualizations/simulation_probabilities.pdf'
 ageProbs = fread(inFileAge)
 sexProbs = fread(inFileSex)
 parityProbs = fread(inFileParity)
+completenessProbs = fread(inFileCompleteness)
 
 # rescale variables
 ageProbs[, unspecified_age:=unspecified_age/sum(unspecified_age)]
 sexProbs[, unspecified_sex:=unspecified_sex/sum(unspecified_sex)]
 parityProbs[, unspecified_parity:=unspecified_parity/sum(unspecified_parity)]
+completenessProbs[, completeness:=completeness/sum(completeness)]
 
 # rename variables
 setnames(ageProbs, 'unspecified_age', 'probabilities')
 setnames(sexProbs, 'unspecified_sex', 'probabilities')
 setnames(parityProbs, 'unspecified_parity', 'probabilities')
+setnames(completenessProbs, 'completeness', 'probabilities')
 
 # format variables
-dts = c('ageProbs', 'sexProbs', 'parityProbs')
+dts = c('ageProbs', 'sexProbs', 'parityProbs', 'completenessProbs')
 for (dt in dts) {
 	tmp = get(dt)
 	tmp[sex=='m', sex:='Male']
@@ -64,12 +68,12 @@ for (dt in dts) {
 # Set up to graph
 
 # titles
-titles = c('Age', 'Sex', 'Parity')
+titles = c('Age', 'Sex', 'Parity', 'Completeness')
 
 # ---------------------------------------------------------------------------
 
 
-# -------------------------------------------------------------------------------
+# --------------------------------------------------------------------
 # Graph
 plots = list()
 for(d in seq_along(dts)) {
@@ -79,11 +83,12 @@ for(d in seq_along(dts)) {
 		facet_grid(sex~parity) + 
 		scale_color_manual('', values=colors) + 
 		scale_fill_manual('', values='white') + 
-		labs(title=paste('Probability of Unspecified', titles[d]), y='Probability', x='Maternal Age') + 
+		labs(title=paste('Probability of Unspecified', titles[d]), 
+			y='Probability', x='Maternal Age') + 
 		theme_bw() + 
 		theme(plot.title=element_text(hjust=.5))
 }
-# -------------------------------------------------------------------------------
+# --------------------------------------------------------------------
 
 
 # -------------------------------------------
