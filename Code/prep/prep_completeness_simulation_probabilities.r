3861f154058aef2412503cd308ec2fc5091dae39
David Phillips <davidp6@uw.edu>
2017-03-30 14:41:57 -0700

    Update completeness probability code

--- Code/prep/prep_completeness_simulation_probabilities.r
+++ Code/prep/prep_completeness_simulation_probabilities.r
@@ -1,10 +1,10 @@
-# ---------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------
 # David Phillips
 #
 # 3/21/2017
-# Prep simulation probabilities for age/sex/parity using Eurostat tabulations
+# Prep simulation probabilities for crude completeness using Eurostat tabulations and estimated births
 # The current working directory should be VSPI_B/
-# ---------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------
 
 
 # ------------------
@@ -18,7 +18,11 @@ library(data.table)
 # Files/directories/lists
 
 # input file
-inFile = './Data/Raw/Other/IFD_coverage_prop_v11495.csv'
+inFile1 = './Data/Country_Data/Country_Year_Age_Sex_Parity_Births.csv'
+inFile2 = './Data/Country_Data/Data 240317.csv'
+
+# country codes
+ccFile = './Data/Country_Data/countrycodes.csv'
 
 # output file
 outFile = './Data/Simulation_Inputs/completeness_probabilities.csv'
@@ -28,8 +32,11 @@ outFile = './Data/Simulation_Inputs/completeness_probabilities.csv'
 # -------------------------------------------------------------------------
 # Load/prep data
 
-# load
-data = fread(inFile)
+# load birth estimates
+estimates = fread(inFile1)
+
+# load data
+data = fread(inFile2)
 
 # drop blank columns/rows
 keepVars = c('Country', 'Year', 'Age', 'Sex', 'Birth order', 'Number births')
@@ -44,69 +51,34 @@ data[sex==3, sex_str:='both']
 data[sex==99, sex_str:='99']
 data$sex = NULL
 setnames(data, 'sex_str', 'sex')
+estimates[, parity:=as.character(parity)]
+
+# bring in iso codes/drop non-GBD countries
+codes = fread(ccFile)
+codes = codes[ihme_indic_country==1, c('iso3', 'countryname'), with=FALSE]
+codes = unique(codes)
+data = merge(data, codes, by.x='country', by.y='countryname')
+data$country = NULL
+
+# merge estimates to data, keep only data that has all variables specified
+setnames(estimates, 'births', 'estimate')
+data = merge(data, estimates, by=c('iso3','year','age','parity','sex'))
 
 # collapse to all countries/years
-denominator = data[, list(births=sum(births, na.rm=TRUE)), by=c('parity', 'age', 'sex')]
-denominator = denominator[parity!='All' & sex!='both'] # drop all/both (not to be confused with unspecified). so far there's no code for all ages
-numerators = denominator[parity=='99' | age==99 | sex=='99']
-denominator = denominator[parity!='99' & age!=99 & sex!='99']
+data = data[, list(births=sum(births), estimate=sum(estimate)), by=c('parity', 'age', 'sex')]
 
-# isolate unspecified counts
-unspecified_age = numerators[age==99 & parity!='99' & sex!='99']
-unspecified_parity = numerators[parity=='99' & age!=99 & sex!='99']
-unspecified_sex = numerators[sex=='99' & parity!='99' & age!=99]
-unspecified_age$age = NULL
-unspecified_parity$parity = NULL
-unspecified_sex$sex = NULL
-setnames(unspecified_age, 'births', 'unspecified_age')
-setnames(unspecified_parity, 'births', 'unspecified_parity')
-setnames(unspecified_sex, 'births', 'unspecified_sex')
-
-# collapse denominators
-age_denominator = denominator[, list(births=sum(births)), by=c('parity', 'sex')]
-sex_denominator = denominator[, list(births=sum(births)), by=c('parity', 'age')]
-parity_denominator = denominator[, list(births=sum(births)), by=c('age', 'sex')]
+# compute crude asp completeness
+data[, completeness:=births/estimate]
+data[completeness>1, completeness:=1]
 # -------------------------------------------------------------------------
 
 
-# -------------------------------------------------------------------------------------
-# Compute probabilities
-
-# merge
-unspecified_age = merge(unspecified_age, age_denominator, by=c('parity', 'sex'))
-unspecified_sex = merge(unspecified_sex, sex_denominator, by=c('parity', 'age'))
-unspecified_parity = merge(unspecified_parity, parity_denominator, by=c('age', 'sex'))
-
-# compute
-unspecified_age[ , unspecified_age:=unspecified_age/births]
-unspecified_sex[ , unspecified_sex:=unspecified_sex/births]
-unspecified_parity[ , unspecified_parity:=unspecified_parity/births]
-
-# drop unnecessary variables
-unspecified_age$births = NULL
-unspecified_sex$births = NULL
-unspecified_parity$births = NULL
-
-# expand across target variable, assume equal probabilities
-# e.g. the probability of being an unknown age is the same for all ages
-ages = unique(denominator$age)
-parities = unique(denominator$parity)
-sexes = unique(denominator$sex)
-expIdxa = rep(seq_len(nrow(unspecified_age)), length(ages))
-expIdxp = rep(seq_len(nrow(unspecified_parity)), length(parities))
-expIdxs = rep(seq_len(nrow(unspecified_sex)), length(sexes))
-unspecified_age = unspecified_age[expIdxa]
-unspecified_parity = unspecified_parity[expIdxp]
-unspecified_sex = unspecified_sex[expIdxs]
-unspecified_age[, age:=rep(ages, each=length(sexes)*length(parities))]
-unspecified_parity[, parity:=rep(parities, each=length(ages)*length(sexes))]
-unspecified_sex[, sex:=rep(sexes, each=length(ages)*length(parities))]
-# -------------------------------------------------------------------------------------
-
-
-# ------------------------------------------------------
+# ----------------------------------------------------------------
 # Save
-write.csv(unspecified_age, outFile1, row.names=FALSE)
-write.csv(unspecified_sex, outFile2, row.names=FALSE)
-write.csv(unspecified_parity, outFile3, row.names=FALSE)
-# ------------------------------------------------------
+
+# subset columns
+data = data[, c('age','sex','parity','completeness'), with=FALSE]
+
+# save
+write.csv(data, outFile, row.names=FALSE)
+# ----------------------------------------------------------------
