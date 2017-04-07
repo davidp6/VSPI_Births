4f146d818cef2ec5aa7a6454be33f72b2a3fda6c
David Phillips <davidp6@uw.edu>
2017-04-07 15:12:05 -0700

    Update code to prep sim pop

--- Code/prep/prep_simulation_population.r
+++ Code/prep/prep_simulation_population.r
@@ -1,85 +1,45 @@
-# ---------------------------------------------------------
+# ------------------------------------------------
 # David Phillips
 #
 # 3/22/2017
-# Generate simulation population from WPP and HFD combined
-# ---------------------------------------------------------
+# Generate simulation population births estimates
+# ------------------------------------------------
 
 
 # ------------------
 # Set up R
 rm(list=ls())
-library(readxl)
 library(data.table)
 # ------------------
 
 
-# ----------------------------------------------------------------------------------
+# --------------------------------------------------------------------
 # Files/directories/lists
 
-# root directory
-root = 'C:/Users/davidp6/Google Drive/Work/Data for Health Initiative/VSPI_Births/'
-
 # input file
-hfdFile = paste0(root, 'Data/Raw/HFD/USAbirthsRRbo.txt')
-wppFile = paste0(root, 'Data/Raw/WPP/SexRatioTotPop-20170322063247.xls')
+inFile = './Data/Country_Data/Country_Year_Age_Sex_Parity_Births.csv'
 
 # output file
-outFile = paste0(root, 'Data/Simulation_Inputs/simulation_population.csv')
-# ----------------------------------------------------------------------------------
+outFile = './Data/Simulation_Inputs/simulation_population.csv'
+# --------------------------------------------------------------------
 
 
-# --------------------------------------------------------------------------------------------
+# ------------------------------------------------
 # Load/prep data
 
-# load HFD data
-hfdData = fread(hfdFile)
+# load data
+data = fread(inFile)
 
-# load WPP data
-wppData = data.table(read_excel(wppFile, sheet='Data', col_names=FALSE))
+# collapse to "global" level
+byVars = c('age','sex','parity')
+data =data[, list(births=sum(births)), by=byVars]
 
-# prep HFD
-hfdData = hfdData[Year>2000]
-hfdData$Total=NULL
-hfdData = melt(hfdData, id.vars=c('Year','Age'), variable.name='parity', value.name='births')
-hfdData[Age=='12-', Age:='12']
-hfdData[Age=='55+', Age:='50']
-hfdData[, Age:=as.numeric(Age)]
-hfdData[, Age:=5*floor(Age/5)]
-hfdData[, parity:=gsub('B', '', parity)]
-hfdData[, parity:=gsub('p', '', parity)]
-hfdData[, parity:=as.numeric(parity)]
-
-# prep WPP sex ratios
-wppData = melt(wppData[3])
-sex_ratio = mean(wppData[11:14]$value)/100
-
-# rename
-setnames(hfdData, 'Age', 'age')
-# --------------------------------------------------------------------------------------------
-
-
-# -------------------------------------------------------------------------
-# Compute population
-
-# cap at parity 4+
-hfdData[parity>4, parity:=4]
-
-# collapse HFD to all years by age/parity
-hfdData = hfdData[, list(births=sum(births)), by=c('age','parity')]
-
-# multiply by global sex ratio to split
-# ratio is m/f, so m/f=ratio and m+f=T, so (1+ratio)*f=T or f=T/(1+ratio)
-hfdData[, f:=births/(sex_ratio+1)]
-hfdData[, m:=births-f]
-
-# reshape long
-hfdData = melt(hfdData, id.vars=c('age', 'parity'), measure.vars=c('m','f'), 
-				variable.name='sex', value.name='births')
-# -------------------------------------------------------------------------
-
-
+# reduce scale for storage
+data[, births:=round(births/1000)]
 # ------------------------------------------------
+
+
+# ---------------------------------------------
 # Save
-write.csv(hfdData, file=outFile, row.names=FALSE)
-# ------------------------------------------------
+write.csv(data, file=outFile, row.names=FALSE)
+# ---------------------------------------------
