e50d5da584527e10c5420030c45dde8e5236da09
David Phillips <davidp6@uw.edu>
2017-09-06 13:44:13 -0700

    Update envelope prep code to reflect new DHS-WPP strategy

--- Code/envelope/set_up_envelope_data.r
+++ Code/envelope/set_up_envelope_data.r
@@ -9,6 +9,7 @@
 # ------------------
 # Set up R
 rm(list=ls())
+library(readxl)
 library(data.table)
 # ------------------
 
@@ -17,10 +18,11 @@ library(data.table)
 # Files/directories/lists
 
 # input file
-inFilecya = './Data/Country_Data/WPP_Country_Year_Age_Estimates.csv'
-inFilecys = './Data/Country_Data/WPP_Country_Year_Sex_Estimates.csv'
-inFiletfr = './Data/Country_Data/WPP_Country_Year_TFR_Estimates.csv'
-inFilecyap = './Data/Country_Data/HFD_Country_Year_Age_Parity_Estimates.csv'
+inFileWPP = './Data/Country_Data/WPP_Country_Year_Age_Estimates.csv'
+inFileDHS = './Data/Country_Data/Birth Data_final.xlsx'
+
+# country codes
+ccFile = './Data/Country_Data/countrycodes.csv'
 
 # output file
 outFile = './Data/Country_Data/Envelope_Input.csv'
@@ -31,27 +33,49 @@ outFile = './Data/Country_Data/Envelope_Input.csv'
 # Load/prep data
 
 # load country-year-age births
-cya = fread(inFilecya)
+WPP = fread(inFileWPP)
 
-# load country-year-sex ratios
-cys = fread(inFilecys)
+# load country-year-age-sex-parity-birthweight numbers from DHS
+DHS = data.table(read_excel(inFileDHS))
 
-# load country-year TFR
-tfr = fread(inFiletfr)
+# fix data prep error in birthyears
+DHS[b2<100 & b2>80, b2:=b2+1900]
 
-# load country-year-age-parity births (HFD is high-income only)
-cyap = fread(inFilecyap)
+# combine 10-14 and 15-19 age groups to match WPP
+DHS[mothage==10, mothage:=15]
 
-# compute proportion of births in each parity by cya
-cyap[, parity_proportion:=births/sum(births), by=c('iso3','year','age')]
-cyap$births = NULL
-cyap = dcast.data.table(cyap, iso3+year+age+country+super_region~parity)
-setnames(cyap, c('1','2','3','4'), paste0('parity_proportion', seq(4)))
+# collapse DHS to the cyaspb level (some surveys overlap in time)
+byVars = c('country','b2','mothage','sex','bord1','birthweight')
+DHS = DHS[, list(births=sum(sample_weight)), by=byVars]
+setnames(DHS, c('b2','mothage','bord1'), c('year','age','parity'))
 
-# merge everything together
-data = merge(cya, cys, by=c('iso3','year','country','super_region'))
-data = merge(data, tfr, by=c('iso3','year','country','super_region'))
-data = merge(data, cyap, by=c('iso3','year','age','country','super_region'), all.x=TRUE)
+# drop unknown birthweight and assume MCAR
+DHS = DHS[!birthweight %in% c('Don\'t know', 'Not weighed at birth')]
+
+# compute proportion of births in each sex-parity-birthweight by cya
+DHS[, prop:=births/sum(births), by=c('country','year','age')]
+DHS[, sample_size:=sum(births), by=c('country','year','age')]
+DHS$births = NULL
+
+# bring in iso codes
+cc = fread(ccFile)
+cc = unique(cc[!is.na(iso3),c('countryname', 'iso3'),with=FALSE])
+DHS = merge(DHS, cc, by.x='country', by.y='countryname', all.x=TRUE)
+DHS$country = NULL
+
+# expand WPP data to "square"
+isos = unique(c(WPP$iso3, DHS$iso3))
+years = unique(c(WPP$year, DHS$year))
+ages = unique(c(WPP$age, DHS$age))
+sexes = unique(DHS$sex)
+parities = unique(DHS$parity)
+birthweights = unique(DHS$birthweight)
+ids = expand.grid(isos, years, ages, sexes, parities, birthweights)
+setnames(ids, c('Var4','Var5','Var6'), c('sex','parity','birthweight'))
+data = merge(WPP, ids, by.x=c('iso3','year','age'), by.y=c('Var1','Var2','Var3'), all.y=TRUE)
+
+# merge proportions to birth numbers
+data = merge(data, DHS, by=c('iso3','year','age','sex','parity','birthweight'), all.x=TRUE)
 # --------------------------------------------------------------------------------------
 
 
