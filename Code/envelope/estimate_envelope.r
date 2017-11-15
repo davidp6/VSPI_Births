55349fd46d37b7a06caf9f1a4be9f7a9a184f7ec
David Phillips <davidp6@uw.edu>
2017-11-15 14:16:20 -0800

    Update envelop estimation to use SUR

--- Code/envelope/estimate_envelope.r
+++ Code/envelope/estimate_envelope.r
@@ -6,9 +6,6 @@
 # Takes data from set_up_envelope_data.r and applies a function
 # ------------------------------------------------------------
 
-# To Do:
-# Maybe SUR is still the right way to go so it can assure that the proportions sum to 1?
-
 
 # ------------------
 # Set up R
@@ -18,6 +15,7 @@ library(stringr)
 library(boot)
 library(lme4)
 library(merTools) # for prediction intervals
+library(systemfit)
 # ------------------
 
 
@@ -42,11 +40,6 @@ outFile = paste0('./Data/Envelopes/Envelope', run_name, '.csv')
 # Load data
 data = fread(inFile)
 
-# drop super regions with no data
-data = data[, all_missing:=all(is.na(prop)), by='super_region']
-data = data[all_missing==FALSE]
-data$all_missing = NULL
-
 # logit transform
 quantiles = quantile(data[prop>0 & prop<1]$prop, c(.025, .975), na.rm=TRUE)
 data[, logit_prop:=logit(prop)]
@@ -63,11 +56,32 @@ data[prop==1, logit_prop:=logit(quantiles[2])]
 				# (1 | super_region) + (1 | sex) + (1 | birthweight), data)
 				
 # run OLS with full interactions
-fit = lm(logit_prop ~ super_region*year*age*sex*parity*birthweight, data)
+# fit = lm(logit_prop ~ super_region*year*age*sex*parity*birthweight, data)
+
+# run SUR with full interactions
+# SUR formulae
+wide = dcast(data, super_region+iso3+year+age~sex+parity+birthweight, value.var='logit_prop')
+vars = names(wide)[!names(wide) %in% c('super_region','iso3','year','age')]
+setnames(wide, vars, paste0('p',seq(length(vars))))
+system = lapply(paste0('p',seq(length(vars))), function(x) { as.formula(paste(x, '~super_region*year*age')) })
+sFit = systemfit(system, data=wide)
 
 # predict
 # data[, c('pred','upper','lower'):=predictInterval(lmeFit, newdata=data, n.sims=2)] # really slow
-data[, 'pred':=predict(fit, newdata=data)]
+# data[, 'pred':=predict(fit, newdata=data)]
+preds = predict(sFit, newdata=wide)
+wide = cbind(wide, preds)
+long = melt(wide, id.vars=c('super_region','iso3','year','age'), measure.vars=names(preds))
+vars = data.table(vars)
+vars[, c('sex','parity','birthweight'):=tstrsplit(vars, '_', fixed=TRUE)]
+vars = cbind(vars, names(preds))
+long = merge(long, vars, by.x='variable', by.y='V2')
+long$vars = NULL
+long$variable = NULL
+setnames(long, 'value', 'pred')
+long[, sex:=as.integer(sex)]
+long[, parity:=as.integer(parity)]
+data = merge(data, long, by=c('iso3','year','age','sex','parity','birthweight'))
 
 # rescale to sum to 1
 data[, pred:=inv.logit(pred)]
